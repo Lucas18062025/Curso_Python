9f2f192bea7b702d393ecd96908bea224c94c238
Lucas18062025 <lucaslean1806@gmail.com>
2026-09-13 13:27:50 -0300

    Fusion Practicas.py: comparadores, ternario, logicos, for/while, app_notas, hola_lucas + LICENSE MIT; fix typo 03-format-strings; remove junk IDE

--- 03-formart-springs.py
+++
@@ -1,5 +0,0 @@
-nombre = "Lucas Leandro"
-apellido = "Villagra"
-nombre_completo = nombre + " " + apellido
-print (nombre_completo)
-print = f"{nombre} {apellido}"
--- Untitled-1.py
+++
@@ -1,6 +0,0 @@
-# Para instalar fastapi, ejecuta en la terminal:
-# Pip install fastapi
-# Para instalar uvicorn, ejecuta en la terminal:
-# Pip install uvicorn
-# Para ejecutar el servidor, ejecuta en la terminal:
-# uvicorn proyectos:app --reload
